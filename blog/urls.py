816fd9386b7653ff8d81e0c43d3c8322ee0da7a5
jvcarpena <jvcarpena2@gmail.com>
2025-01-29 15:17:47 +0800

    Configure the urls, views, and urls of the main project

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+url_patterns = [
+    path("", views.index, name="home-page"),
+    path("posts", views.posts, name="posts-page"),
+    path("posts/<slug:slug>", views.post_detail,
+         name="post-detail-page")  # posts/my-first-post
+]
---
+++ blog/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import Http404, HttpResponseRedirect
+from django.urls import reverse
+
+# Create your views here.
+
+
+def index(request):
+    pass
+
+
+def posts(request):
+    pass
+
+
+def post_detail(request, slug):
+    pass
