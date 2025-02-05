0f560f023b55bbca5af6dde66f02a9c082f6dfb2
jvcarpena <jvcarpena2@gmail.com>
2025-02-05 17:59:18 +0800

    Finished the blog project that uses database instead of hardcoded data

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,16 @@
-from django.shortcuts import render
-from datetime import date
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 # Create your views here.
 
-all_posts = Post.objects.all()
-
-
-def get_date(post):
-    return post['date']
-
-
 def index(request):
-    latest_post = all_posts.order_by("-date")[:3]
+    latest_post = Post.objects.all().order_by("-date")[:3]
     return render(request, "blog/index.html", {
         "posts": latest_post
     })
 
 
 def posts(request):
+    all_posts = Post.objects.all().order_by("-date")
     return render(request, "blog/all-posts.html", {
         "posts": all_posts
     })
@@ -26,10 +19,8 @@ def posts(request):
 def post_detail(request, slug):
     # list comprehension using next function.
     # post_to_show = next(post for post in all_posts if post['slug'] == slug)
-    post_to_show = None
-    for item in all_posts:  # Note that all_posts is an object from the Post Model.
-        if item.slug == slug:
-            post_to_show = item
+    post_to_show = get_object_or_404(Post, slug=slug)
     return render(request, "blog/post-detail.html", {
-        "post": post_to_show
+        "post": post_to_show,
+        "post_tags": post_to_show.tag.all()
     })
