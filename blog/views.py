15503ac2b028e7317509103291e47a693ed81d25
jvcarpena <jvcarpena2@gmail.com>
2025-02-05 17:32:07 +0800

    Configured the index.html and the view that handles that page

--- blog/views.py
+++ blog/views.py
@@ -1,49 +1,10 @@
 from django.shortcuts import render
 from datetime import date
+from .models import Post
 # Create your views here.
 
 all_posts = [
-    {
-        "slug": "sakamoto-days",
-        "image": "sakamoto.jpg",
-        "author": "JV",
-        "date": date(2025, 1, 25), 
-        "title": "Sakamoto Days: Action Comedy",
-        "excerpt": "A legendary hitman retires to run a convenience store, but his past won’t let him go. Sakamoto Days delivers intense action, sharp comedy, and a lovable cast in a thrilling, must-watch anime.",
-        "content": """
-            Taro Sakamoto, once the world’s deadliest assassin, now lives a peaceful life with his family—until 
-            old enemies come knocking. Though out of shape, his combat skills remain unmatched, leading to 
-            high-energy battles mixed with absurd humor. From explosive fights to hilarious everyday struggles, 
-            Sakamoto Days masterfully blends action and comedy, making it a standout in the genre.
-        """
-    },
-    {
-        "slug": "python-for-everyone",
-        "image": "coding.jpg",
-        "author": "JV",
-        "date": date(2025, 1, 27),
-        "title": "Python: Simple Yet Powerful",
-        "excerpt": "Python is a beginner-friendly yet powerful language used in web development, data science, AI, and automation.",
-        "content": """
-            With its easy-to-read syntax and vast libraries, Python is perfect for both beginners and experts. 
-            Whether building websites, analyzing data, or automating tasks, Python’s versatility and strong community 
-            make it a top choice for developers.
-        """
-    },
-    {
-        "slug": "kobe-bean-bryant",
-        "image": "kobe.jpg",
-        "author": "JV",
-        "date": date(2025, 1, 29),
-        "title": 'Kobe: A Legacy of Greatness',
-        "excerpt": 'Kobe Bryant wasn’t just a basketball legend—he was a symbol of hard work, determination, and the “Mamba Mentality.”',
-        "content": """
-            With five NBA championships, an MVP award, and countless unforgettable moments, 
-            Kobe’s impact on the game was unmatched. His relentless work ethic and passion inspired athletes 
-            worldwide, proving that success comes from dedication and perseverance. Even beyond basketball, 
-            his influence in storytelling and mentoring continues to inspire future generations.
-        """
-    }
+    
 ]
 
 
@@ -52,8 +13,7 @@ def get_date(post):
 
 
 def index(request):
-    sorted_post = sorted(all_posts, key=get_date)
-    latest_post = sorted_post[-3:]
+    latest_post = Post.objects.all().order_by("-date")[:3]
     return render(request, "blog/index.html", {
         "posts": latest_post
     })
